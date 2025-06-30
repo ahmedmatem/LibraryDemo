7bc170373fe62d6a34c25f0a2dd878b868eb5348
Ahmed Matem <ahmedmatem@gmail.com>
2025-06-30 20:43:12 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            DisplayMenu();
+        }
+        private static void DisplayMenu()
+        {
+            // TODO: Implement display menu
+            throw new NotImplementedException();
         }
     }
 }
